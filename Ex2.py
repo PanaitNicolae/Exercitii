03f5db4706a2bf83b78b616aef06327dc8fec780
PanaitNicolae <84705549+PanaitNicolae@users.noreply.github.com>
2021-05-24 12:27:32 +0300

    Update Ex2.py

--- Ex2.py
+++ Ex2.py
@@ -10,12 +10,15 @@ go_button_path = "//button[@id='submit']"
 first_result_link_path = "//ul[@class='list-recent-events menu']/li[1]/h3/a[contains(text(), 'Decorators for Functions')]"
 examples_link_path = "//div[@class='contents topic']/ul/li/a[text()='Examples']"
 examples_path = "//div[@id='examples']//li"
+
 search_bar = driver.find_element(By.XPATH, search_bar_path )
 go_button = driver.find_element(By.XPATH, go_button_path)
+
 search_bar.send_keys("decorator")
 go_button.click()
 driver.find_element(By.XPATH, first_result_link_path).click()
 driver.find_element(By.XPATH, examples_link_path).click()
+
 examples_number = len(driver.find_elements(By.XPATH, examples_path))
 
 if examples_number == 5:
