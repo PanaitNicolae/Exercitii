8d25fcb462e375649813cc2a3b75c03398226612
PanaitNicolae <84705549+PanaitNicolae@users.noreply.github.com>
2021-06-17 11:36:21 +0300

    Update TextBoxes.py

--- Demoqa.com/TextBoxes.py
+++ Demoqa.com/TextBoxes.py
@@ -1,40 +1,47 @@
-from selenium import webdriver
 import time
+from Driver import *
 
-thisdict = {
-  "Full Name": "First Test",
-  "Email": "abcd@abc.com",
-  "Current Address": "Prahova",
-  "Permanent Address": "Prahova"
-}
+class TextBoxes(Driver):   
 
-driver = webdriver.Chrome()
+    def click_submit(self):
+        submit_btn_xpath = "//button[@id='submit']"
+        submit_btn = self._driver().find_element_by_xpath(submit_btn_xpath)
+        submit_btn.click()
+        
+        
 
+    def find_labels(self):
+        generic_xpath = "//div[contains(@id, 'wrapper')]"
+        labels_xpath = generic_xpath + "//label"
+        elements = self._driver().find_elements_by_xpath(labels_xpath)
+        available_labels = []
+        for i in range(len(elements)):
+           available_labels.append(elements[i].text)
 
-def navigate_to(url = "https://demoqa.com/text-box"):
-    driver.get(url)
-      
-def click_submit():
-    submit_btn_xpath = "//button[@id='submit']"
-    submit_btn = driver.find_element_by_xpath(submit_btn_xpath)
-    submit_btn.click()
+        return available_labels
 
-def complete_textboxes(form_dict = thisdict):    
-    generic_xpath = "//div[contains(@id, 'wrapper')]"
-    labels_xpath = generic_xpath + "//label"
-    input_xpath = "//div[contains(@id, 'wrapper') and .//label[text()='{}']]//input | //div[contains(@id, 'wrapper') and .//label[text()='{}']]//textarea" 
-    elements = driver.find_elements_by_xpath(labels_xpath)
-    available_labels = []
-    for i in range(len(elements)):
-       available_labels.append(elements[i].text)
-    print(available_labels) 
-    
-    for i in form_dict.keys():
-        if i in available_labels:
-            input_text = driver.find_element_by_xpath(input_xpath.format(i, i))
-            input_text.send_keys(form_dict[i])
-                
-     
-navigate_to()
-complete_textboxes(thisdict) 
-click_submit()    
+    def complete_textboxes(self, form_dict):    
+        input_xpath = "//div[contains(@id, 'wrapper') and .//label[text()='{}']]//input | \
+                      //div[contains(@id, 'wrapper') and .//label[text()='{}']]//textarea" 
+        available_labels = self.find_labels()
+        for i in list(form_dict.keys()):
+            if i in available_labels:
+                input_text = self._driver().find_element_by_xpath(input_xpath.format(i, i))
+                input_text.send_keys(form_dict[i])
+            
+
+    def get_results(self, form_dict):
+        results_xpath = "//div[@id='output']//p"
+        results = self.driver.find_elements_by_xpath(results_xpath)
+        keys = form_dict.keys()
+        values = []
+        for i in results:
+            result = i.text
+            values.append(result.split(":")[1])
+        return dict(zip(keys, values))
+
+    def check_results(self, form_dict):
+        results = self.get_results(form_dict)
+        if not results == form_dict:
+            raise Exception("Wrong results!")
+        print("Checked!")
